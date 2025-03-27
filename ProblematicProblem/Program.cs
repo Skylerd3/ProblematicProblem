6bdf6a12da3740384d7d8adb708455e35e9e40f1
skyler dennis <skylerdenniss11@gmail.com>
2025-03-27 16:50:28 -0400

    I fixed all the misspellings

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -12,17 +12,17 @@ namespace ProblematicProblem
         static void Main(string[] args)
         {
             Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
-            var UserResponce = Console.ReadLine().ToLower();
+            var UserResponse = Console.ReadLine().ToLower();
 
-            while (UserResponce != "yes" && UserResponce != "no")
+            while (UserResponse != "yes" && UserResponse != "no")
             {
-                Console.WriteLine("Invalid responce, Please type yes or no.");
-                UserResponce = Console.ReadLine().ToLower();
+                Console.WriteLine("Invalid response, Please type yes or no.");
+                UserResponse = Console.ReadLine().ToLower();
             }
 
-            if (UserResponce == "no")
+            if (UserResponse == "no")
             {
-                Console.WriteLine("You salected NO, Thanks for playsing.");
+                Console.WriteLine("You selected NO, Thanks for playing.");
                 return;
             }
 
@@ -40,18 +40,18 @@ namespace ProblematicProblem
 
             Console.WriteLine();
             Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-            UserResponce = (Console.ReadLine().ToLower());
+            UserResponse = (Console.ReadLine().ToLower());
 
-            while (UserResponce != "sure" && UserResponce != "no thanks")
+            while (UserResponse != "sure" && UserResponse != "no thanks")
             {
-                Console.WriteLine("Invlaid responce, Please typ sure or no thanks!");
-                UserResponce = Console.ReadLine().ToLower();
+                Console.WriteLine("Invlaid response, Please type sure or no thanks!");
+                UserResponse = Console.ReadLine().ToLower();
             }
 
 
 
 
-            if (UserResponce == "sure")
+            if (UserResponse == "sure")
             {
 
 
@@ -65,15 +65,15 @@ namespace ProblematicProblem
 
                 Console.WriteLine();
                 Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-                UserResponce = Console.ReadLine().ToLower();
+                UserResponse = Console.ReadLine().ToLower();
 
-                while (UserResponce != "yes" && UserResponce != "no")
+                while (UserResponse != "yes" && UserResponse != "no")
                 {
-                    Console.WriteLine("Invalid responce, Pleasy type yes or no.");
-                    UserResponce = Console.ReadLine().ToLower();
+                    Console.WriteLine("Invalid response, Please type yes or no.");
+                    UserResponse = Console.ReadLine().ToLower();
                 }
 
-                while (UserResponce == "yes")
+                while (UserResponse == "yes")
                 {
                     string userAddition = Console.ReadLine();
                     activities.Add(userAddition);
@@ -85,12 +85,12 @@ namespace ProblematicProblem
 
                     Console.WriteLine();
                     Console.WriteLine("Would you like to add more? yes/no: ");
-                    UserResponce = Console.ReadLine().ToLower();
+                    UserResponse = Console.ReadLine().ToLower();
 
-                    while (UserResponce != "yes" && UserResponce != "no")
+                    while (UserResponse != "yes" && UserResponse != "no")
                     {
-                        Console.WriteLine("Invalid responce, Pleasy type yes or no.");
-                        UserResponce = Console.ReadLine().ToLower();
+                        Console.WriteLine("Invalid response, Please type yes or no.");
+                        UserResponse = Console.ReadLine().ToLower();
                     }
                 }
             }
@@ -123,14 +123,14 @@ namespace ProblematicProblem
                 }
                 Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
                 Console.WriteLine();
-                UserResponce = Console.ReadLine().ToLower();
+                UserResponse = Console.ReadLine().ToLower();
 
-                while (UserResponce != "keep" && UserResponce != "redo")
+                while (UserResponse != "keep" && UserResponse != "redo")
                 {
-                    Console.WriteLine("Invalid responce, Pleasy type keep or redo.");
-                    UserResponce = Console.ReadLine().ToLower();
+                    Console.WriteLine("Invalid response, Please type keep or redo.");
+                    UserResponse = Console.ReadLine().ToLower();
                 }
-                if (UserResponce == "keep")
+                if (UserResponse == "keep")
                 {
                     cont = false;
                 }
